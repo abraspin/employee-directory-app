931ea20f13c7f068b0a34f4a14acc71805d7ce24
abraspin <abrahamspindel@gmail.com>
2020-12-05 14:48:49 -0600

    add api export for randomusersAPI fetch

---
+++ src/utils/API.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+const BASEURL = "https://randomuser.me/api/?inc=name,email,picture,phone&results=";
+
+export default {
+  getUsers: async function (resultsQty) {
+    return await axios.get(BASEURL + resultsQty);
+  },
+};
