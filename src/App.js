cb70286147c6da978ed68d343cc070ec07bfe929
abraspin <abrahamspindel@gmail.com>
2020-12-05 14:48:16 -0600

    add fetch for random users

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+// import './App.css';
+import FriendCard from "./components/FriendCard";
+import HeaderBar from "./components/HeaderBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import API from "./utils/API.js";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [userList, setUserList] = React.useState([]);
+
+  React.useEffect(() => {
+    API.getUsers(10).then((res) => {
+      console.log("🚀 ~ file: App.js ~ line 16 ~ API.getUsers ~ res.data.results", res.data.results);
+      setUserList(res.data.results);
+    });
+  });
+  console.log("🚀 ~ file: App.js ~ line 19 ~ App ~ userList", userList);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <HeaderBar title="Employee Directory" icon="fas fa-user"></HeaderBar>
       </header>
+      <section></section>
     </div>
   );
 }
