bbc16809636be5f2983e9f2b06dd4093fc19cb1c
abraspin <abrahamspindel@gmail.com>
2020-12-07 21:11:57 -0600

    move sortableData hook and update search filter to reset userList each time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import UserCard from "./components/UserCard";
+// import UserCard from "./components/UserCard";
 import HeaderBar from "./components/HeaderBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import API from "./utils/API.js";
@@ -12,51 +12,26 @@ function App() {
   const [userList, setUserList] = useState([]);
   const [employeeDirectory, setEmployeeDirectory] = useState();
 
-  // moving this to userTable comp
-  const [searchTerm, setSearchTerm] = useState();
-
-  // $(".searchBox").value = "";
   function searchHandler(searchTerm) {
-    console.log("🚀 ~ file: App.js ~ line 18 ~ App ~ searchTerm", searchTerm);
-    // event.preventDefault();
-    setSearchTerm(searchTerm);
-    // trying something crazy...(global...constant...??)
-    setUserList(employeeDirectory);
-    // let employeeDataSomething = [];
-    if (!searchTerm) {
-      setUserList(employeeDirectory);
-    } else {
-      let employeeDataSomething = [];
-      for (let i = 0; i < userList.length; i++) {
-        if (userList[i].name.indexOf(searchTerm) > -1) {
-          employeeDataSomething.push(userList[i]);
-        }
-      }
-      setUserList(employeeDataSomething);
-    }
+    ///////////////////////////////////////////////////////////////////////////////////////
+    const filteredArray = employeeDirectory.filter((a) => a.name.toLowerCase().includes(searchTerm));
+    setUserList(filteredArray);
+    ///////////////////////////////////////////////////////////////////////////////////////
+
+    // let arrayToFilter = [...employeeDirectory];
+    // let filteredArray = [];
+    // for (let i = 0; i < arrayToFilter.length; i++) {
+    //   if (arrayToFilter[i].name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+    //     filteredArray.push(arrayToFilter[i]);
+    //   }
+    // }
+    // setUserList(arrayToFilter);
+    ///////////////////////////////////////////////////////////////////////////////////////
   }
 
-  //FIXME: this is an attempt at adding the mdbootstrap table functionality, it's in index.html ATM
-  // useEffect(() => {
-  //   const script = document.createElement("script");
-
-  //   script.src = "https://use.typekit.net/foobar.js";
-  //   script.async = true;
-
-  //   document.body.appendChild(script);
-
-  //   return () => {
-  //     document.body.removeChild(script);
-  //   };
-  // }, []);
-
-  //   document.getElementById("dtBasicExample").DataTable();
-  //   document.getElementsByClassName("dataTables_length").addClass("bs-select");
-
+  //Query randomuser API and create a new array with just the key/value pairs we want
   useEffect(() => {
     API.getUsers(20).then((res) => {
-      //manipulate fields here maybe
-      // JSON object - name, email,. dob, then concatenate firstName lastName
       let employeesArray = [];
       for (let i = 0; i < res.data.results.length; i++) {
         let employee = {
@@ -72,38 +47,10 @@ function App() {
     });
   }, []);
 
-  // TODO: Should this be wrapped in a useEffect with Observe[searchTerm] ?
-  // if (searchTerm) {
-  //   //TODO: pretty sure this is all trash
-  //   // const userListCopy = [...userList];
-  //   // const filteredUserList = userListCopy.filter((user) => {
-  //   //   // const userFullName = user.name.first + " " + user.name.last;
-  //   //   (user.name.first + " " + user.name.last).toLowerCase().includes(searchTerm.toLowerCase());
-  //   // });
-
-  //   ///////////////////////////////////////////////////////////////////////////////////////////
-
-  //   // FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:FIXME:
-  //   const filteredUserList = [];
-  //   userList.forEach((user) => {
-  //     const userFullName = user.name;
-  //     console.log("🚀 ~ file: App.js ~ line 50 ~ userList.forEach ~ userFullName", userFullName);
-  //     const nameToLower = userFullName.toLowerCase;
-  //     console.log(userFullName.toLowerCase().includes(searchTerm.toLowerCase()));
-
-  //     const searchTermToLower = searchTerm.toLowerCase;
-  //     if (nameToLower.includes(searchTermToLower)) {
-  //       filteredUserList.push(user);
-  //     }
-  //   });
-  //   console.log("🚀 ~ file: App.js ~ line 34 ~ filteredUserList ~ filteredUserList", filteredUserList);
-  //   //TODO: Then just map the filtered list in the render funciton instead of userList...gg
-  // }
   return (
     <div>
       <header>
         <HeaderBar title="Employee Directory" icon="fas fa-user"></HeaderBar>
-        {/* moving this to the table  */}
         <div className="row bg-dark">
           <div className="col md-8">
             <h3 className="text-secondary mx-auto py-4 px-4">Click on the column header to sort users</h3>
@@ -116,95 +63,10 @@ function App() {
         </div>
       </header>
       <section>
-        {/* ////////////////////line below for userList component homemade/////////////////////////// */}
         <UserTable userList={userList}></UserTable>
-        {/* ////////////////////block below for MDBootstrap sortable table with userList/////////////////////////// */}
-        {/* <table id="dtBasicExample" className="table table-striped table-bordered table-sm" cellSpacing="0" width="100%">
-          <thead>
-            <tr>
-              <th className="th-sm">User</th>
-              <th className="th-sm">Name</th>
-              <th className="th-sm">Email</th>
-              <th className="th-sm">Phone</th>
-            </tr>
-          </thead>
-          <tbody>
-            {userList.map((user) => {
-              return (
-                <tr>
-                  <td>
-                    <img src={user.picture.medium} alt={user.name.first + " " + user.name.last} />
-                  </td>
-                  <td>{user.name.first + " " + user.name.last}</td>
-                  <td>{user.email}</td>
-                  <td>{user.phone}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table> */}
-        {/* ////////////////////block above for MDBootstrap sortable table with userList/////////////////////////// */}
-        {/* //////////////////////////////////////////block below MDBootstrap sortable table ///////////////////////////////////////////////////////////////// */}
-        {/* <table id="dtBasicExample" className="table table-striped table-bordered table-sm" cellSpacing="0" width="100%">
-          <thead>
-            <tr>
-              <th className="th-sm">Name</th>
-              <th className="th-sm">Position</th>
-              <th className="th-sm">Office</th>
-              <th className="th-sm">Age</th>
-              <th className="th-sm">Start date</th>
-              <th className="th-sm">Salary</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Tiger Nixon</td>
-              <td>System Architect</td>
-              <td>Edinburgh</td>
-              <td>61</td>
-              <td>2011/04/25</td>
-              <td>$320,800</td>
-            </tr>
-            <tr>
-              <td>Tiger Nixon</td>
-              <td>System Architect</td>
-              <td>Edinburgh</td>
-              <td>61</td>
-              <td>2011/04/25</td>
-              <td>$320,800</td>
-            </tr>
-          </tbody>
-        </table> */}
-        {/* ////////////////////block above for MDBootstrap sortable table/////////////////////////// */}
       </section>
     </div>
   );
 }
 
 export default App;
-
-////////// removed table from  section element, to be component
-// <table className="table table-striped table-bordered text-center ">
-// <thead>
-//   <tr>
-//     {/* begin TableHeader component refactor */}
-//     <th scope="col">User</th>
-//     {/* end TableHeader component refactor */}
-//     <th scope="col">Name</th>
-//     <th scope="col">Email</th>
-//     <th scope="col">Phone</th>
-//   </tr>
-// </thead>
-// <tbody>
-//   {userList.map((user) => {
-//     return (
-//       <UserCard
-//         name={user.name.first + " " + user.name.last}
-//         email={user.email}
-//         phone={user.phone}
-//         picture={user.picture.medium}
-//       />
-//     );
-//   })}
-// </tbody>
-// </table>
--- src/components/UserTable/index.js
+++ src/components/UserTable/index.js
@@ -1,72 +1,10 @@
 import React from "react";
 import UserCard from "../UserCard";
-import SearchBox from "../SearchBox";
-
-// custom hook used to handle sort requests
-const useSortableData = (items, config = null) => {
-  // sortConfig state is an object with keys fieldName and direction (asc vs des)
-  const [sortConfig, setSortConfig] = React.useState(config);
-  const [sortDirection, setSortDirection] = React.useState();
-
-  // memoize the sort operation so it does not have to re-sort every render...what's the difference?
-  const sortedItems = React.useMemo(() => {
-    // const sortedItems = React.useEffect(() => { //this will not work
-    // copy the array because array.sort will modify the original array
-    let sortableItems = [...items];
-    console.log("🚀------ ~ file: index.js ~ line 15 ~ //sortedItems ~ sortableItems", sortableItems);
-
-    // if no sort config has been set, return the array unchanged
-    if (sortConfig !== null) {
-      sortableItems.sort((a, b) => {
-        // TODO: get this: How is it that each element has its own sortConfig state?
-        if (a[sortConfig.key] < b[sortConfig.key]) {
-          // If it's already ascending and direction is ascending, return -1 (aka a is smaller than b)
-          return sortConfig.direction === "ascending" ? -1 : 1;
-        }
-        if (a[sortConfig.key] > b[sortConfig.key]) {
-          return sortConfig.direction === "ascending" ? 1 : -1;
-        }
-        return 0;
-      });
-
-      //////////////////////////////////////
-      // if (sortConfig.direction === "ascending") {
-      //   sortableItems.sort((a, b) => {
-      //     return a[sortConfig.key] < b[sortConfig.key];
-      //   });
-      // } else {
-      //   sortableItems.sort((a, b) => {
-      //     return a[sortConfig.key] > b[sortConfig.key];
-      //   });
-      // }
-      //////////////////////////////////////
-    }
-    console.log("🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 ~ file: index.js ~ line 37 ~ sortableItems.sort ~ sortableItems", sortableItems);
-
-    return sortableItems;
-  }, [items, sortConfig]);
-
-  // this is called when the sort is requested by clicking table header
-  const requestSort = (key) => {
-    console.log("🚀 ~ file: index.js ~ line 50 ~ requestSort ~ key", key);
-    let direction = "ascending";
-    // if sortconfig exists AND the thingToSearch matches the requesting thing AND direction is ascending, THEN set to descending (flip it)
-    if (sortConfig && sortConfig.key === key && sortConfig.direction === "ascending") {
-      console.log("I flipped to descending!");
-      direction = "descending";
-    } else {
-      direction = "ascending";
-    }
-    setSortConfig({ key, direction });
-    console.log("I just tried to setSortConfig to: ", sortConfig);
-  };
-
-  return { items: sortedItems, requestSort, sortConfig };
-};
+// import SearchBox from "../SearchBox";
+import useSortableData from "../../utils/useSortableData";
 
 const UserTable = (props) => {
-  // console.log("🚀 ~ file: index.js ~ line 38 ~ UserTable ~ props.userList", props.userList);
-  const { items, requestSort, sortConfig } = useSortableData(props.userList);
+  const { items, requestSort, sortConfig /*searchHandler */ } = useSortableData(props.userList);
   const getClassNamesFor = (name) => {
     if (!sortConfig) {
       return;
@@ -74,51 +12,45 @@ const UserTable = (props) => {
     return sortConfig.key === name ? sortConfig.direction : undefined;
   };
 
-  const [searchTerm, setSearchTerm] = React.useState();
-  const [userList, setUserList] = React.useState([]);
+  // const [searchTerm, setSearchTerm] = React.useState();
+  // const [userList, setUserList] = React.useState([]);
 
   // $(".searchBox").value = "";
-  function searchHandler(searchTerm) {
-    console.log("🚀 ~ file: App.js ~ line 18 ~ App ~ searchTerm", searchTerm);
-    // event.preventDefault();
-    setSearchTerm(searchTerm);
-    if (!searchTerm) {
-      setUserList(props.userList);
-    } else {
-      let employeeDataSomething = [];
-      for (let i = 0; i < userList.length; i++) {
-        if (userList[i].name.indexOf(searchTerm) > -1) {
-          employeeDataSomething.push(userList[i]);
-        }
-      }
-      setUserList(employeeDataSomething);
-    }
-  }
+  // function searchHandler(searchTerm) {
+  //   console.log("🚀 ~ file: App.js ~ line 18 ~ App ~ searchTerm", searchTerm);
+  //   // event.preventDefault();
+  //   // setSearchTerm(searchTerm);
+  //   if (!searchTerm) {
+  //     setUserList(props.userList);
+  //   } else {
+  //     let employeeDataSomething = [];
+  //     for (let i = 0; i < userList.length; i++) {
+  //       if (userList[i].name.indexOf(searchTerm) > -1) {
+  //         employeeDataSomething.push(userList[i]);
+  //       }
+  //     }
+  //     // setUserList(employeeDataSomething);
+  //     items = employeeDataSomething;
+  //   }
+  // }
 
   return (
     <div>
-      <div className="row bg-dark">
+      {/* <div className="row bg-dark">
         <div className="col md-8">
           <h3 className="text-secondary mx-auto py-4 px-4">Click on the column header to sort users</h3>
         </div>
         <div className="col md-4">
           <h3 className="text-secondary mx-auto py-4 px-2">
-            Or search by name: <SearchBox placeholder="enter name..." searchHandler={searchHandler}></SearchBox>
+            Or search by name (table method): <SearchBox placeholder="enter name..." searchHandler={searchHandler}></SearchBox>
           </h3>
         </div>
-      </div>
+      </div> */}
 
       <table className="table table-striped table-bordered text-center ">
         <thead>
           <tr>
-            {/* begin TableHeader component refactor */}
-            {/* <th scope="col">User</th> */}
-            {/* end TableHeader component refactor */}
-            <th>
-              {/* <button type="button" onClick={() => requestSort("user")} className={getClassNamesFor("user")}> */}
-              User
-              {/* </button> */}
-            </th>
+            <th>User</th>
             <th>
               <button type="button" onClick={() => requestSort("name")} className={getClassNamesFor("name")}>
                 Name
@@ -130,9 +62,6 @@ const UserTable = (props) => {
               </button>
             </th>
             <th>Phone</th>
-            {/* <th scope="col">Name</th>
-        <th scope="col">Email</th>
-        <th scope="col">Phone</th> */}
           </tr>
         </thead>
         <tbody>
